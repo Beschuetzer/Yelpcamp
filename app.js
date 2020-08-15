0aa889f81ffabbfca68f5f0978e371a6902baa26
Adam Major <adam.j.major@gmail.com>
2020-08-14 20:10:52 -0700

    Refactored and added Delete route

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express'),
       axios = require('axios'),
       bodyParser = require('body-parser'),
       mongoose = require('mongoose'),
-      dbName = "yelpCamp";
+      methodOverride = require('method-override'),
+      dbName = "yelpCamp",
+      Campground = require('./models/campground.js');
+      
 
 mongoose.connect('mongodb://localhost:27017/' + dbName, {
   useNewUrlParser: true,
@@ -13,24 +16,15 @@ mongoose.connect('mongodb://localhost:27017/' + dbName, {
 .then(() => console.log('Connected to yelpCamp!'))
 .catch(error => console.log(error.message));
 
-//creating a schema for each objecct
-const campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String,
-  alt: String,
-});
-
-//creating the campgrounds collection in the db 'yelpCamp'
-const Campground = mongoose.model("Campground", campgroundSchema);
 //#endregion
 //#region Configuring Express
 //telling express to serve files in 'public'
 app.use(express.static("public"));
 app.use(express.static("views/imgs"));
 app.use(bodyParser.urlencoded([{extended: true}]));
+app.use(methodOverride("_method"));
 
-//allows you to omit .ejs for every file
+d//allows you to omit .ejs for every file
 app.set('view engine', 'ejs');
 
 app.listen(3000, function(){
@@ -51,11 +45,10 @@ app.get('/campgrounds', function(req, res){
     }
     else {
       console.log("Found the following: \n" + returnedItem);
-      res.render('campgrounds', {campgrounds: returnedItem});
+      res.render('index', {campgrounds: returnedItem});
     }
   });
 });
-
 
 app.post('/campgrounds', (req,res) =>{
   console.log(req.body)
@@ -82,7 +75,7 @@ app.post('/campgrounds', (req,res) =>{
       }
       else {
         console.log('toAdd saved to database' + returnedItem);
-        res.redirect('campgrounds');
+        res.redirect('/campgrounds');
       }
     });
   }
@@ -92,20 +85,33 @@ app.post('/campgrounds', (req,res) =>{
 });
 
 app.get('/campgrounds/new', (req,res) => {
-  res.render('new.ejs')
+  res.render('new')
+});
+
+app.get('/campgrounds/:id', (req, res) =>{
+  Campground.findById(req.params.id, (err, foundItem) => {
+    if (err) {
+      console.log("something went wrong finding ID");
+    }
+    else {
+      res.render('show', {campground: foundItem});
+    }
+  });
+});
+
+app.delete('/campgrounds/:id', function (req,res){
+  Campground.findByIdAndDelete(req.params.id, function(err){
+    if (err){
+      console.log(err);
+    }
+  });
+  res.redirect('/campgrounds')
 });
 
 app.get('*', function(req, res) {
   res.send(`Sorry, ${req.originalUrl} page not found...\nWhat are you doing with your life?`);
 });
 //#endregion
-
-// campgrounds = [
-//   {alt: "Fiery Nights", name: "Salmon Creek", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", description: "Light a fire and relax with some friends on this camping experience."},
-//   {alt: "Multi Hued Camping", name: "Social Camping", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", description: "Colorfully selected tents and wonderful company abound on this camping experience."},
-//   {alt: "Starry sky camping", name: "Astronomy View", image: "https://images.pexels.com/photos/2666598/pexels-photo-2666598.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", description: "Ever wanted to be a part of the Universe?  This experience is GALACTIC!"},
-//   {alt: "Camping During the Day", name: "Day Camping", image: "https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", description: "Break free from convention!  Explore your inner rebel!"},
-// ];
 
 
 
